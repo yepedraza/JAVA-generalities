5a4a746f445639302b1b2c26e108f13248f027e5
Yery Pedraza <yery.pedraza523@gmail.com>
2021-08-24 21:45:53 -0500

    Agrego ejercicio de JFreeChart

---
+++ Projects/JFreeChart/src/jfreechart/JFreeChart.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jfreechart;
+
+/**
+ *
+ * @author Yery Pedraza
+ */
+public class JFreeChart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
